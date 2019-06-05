7404d2e61fb3b5c58e3888e91a9521e3c600dc72
Nicky van Urk <nickyvanurk@gmx.com>
2019-06-05 10:56:37 +0200

    Add logout all devices API endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -40,4 +40,15 @@ router.post('/users/logout', auth, async (req, res) => {
   }
 });
 
+router.post('/users/logoutall', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 module.exports = router;
